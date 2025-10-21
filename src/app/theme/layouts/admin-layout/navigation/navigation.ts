b97c60109b0f4764953c4bddcb4526111be97ba7
unknown <tonderaimuchada@gmail.com>
2025-10-21 15:33:52 +0200

    Added template pages for the project

---
+++ src/app/theme/layouts/admin-layout/navigation/navigation.ts
@@ -0,0 +1,76 @@
+export interface NavigationItem {
+  id: string;
+  title: string;
+  type: 'item' | 'collapse' | 'group';
+  translate?: string;
+  icon?: string;
+  hidden?: boolean;
+  url?: string;
+  classes?: string;
+  groupClasses?: string;
+  exactMatch?: boolean;
+  external?: boolean;
+  target?: boolean;
+  breadcrumbs?: boolean;
+  children?: NavigationItem[];
+  link?: string;
+  description?: string;
+  path?: string;
+}
+
+export const NavigationItems: NavigationItem[] = [
+  {
+    id: 'dashboard',
+    title: 'Dashboard',
+    type: 'group',
+    icon: 'icon-navigation',
+    children: [
+      {
+        id: 'customers',
+        title: 'Customers',
+        type: 'item',
+        classes: 'nav-item',
+        url: '/dashboard/default',
+        icon: 'dashboard',
+        breadcrumbs: false
+      },
+      {
+        id: 'product',
+        title: 'Products',
+        type: 'item',
+        classes: 'nav-item',
+        url: '/dashboard/products', //url: '/sample-page',
+        icon: 'chrome',
+        breadcrumbs: false
+      }
+    ]
+  },
+  {
+    id: 'authentication',
+    title: 'Authentication',
+    type: 'group',
+    icon: 'icon-navigation',
+    children: [
+      {
+        id: 'login',
+        title: 'Login',
+        type: 'item',
+        classes: 'nav-item',
+        url: '/login',
+        icon: 'login',
+        target: true,
+        breadcrumbs: false
+      },
+      {
+        id: 'register',
+        title: 'Register',
+        type: 'item',
+        classes: 'nav-item',
+        url: '/register',
+        icon: 'profile',
+        target: true,
+        breadcrumbs: false
+      }
+    ]
+  }
+];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+import { environment } from './environments/environment';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { AppRoutingModule } from './app/app-routing.module';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppComponent } from './app/app.component';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(BrowserModule, AppRoutingModule), provideAnimations()]
+}).catch((err) => console.error(err));
